46f97b04556d88f29a41b81f9d2772481a1d3382
miracy1lmaz <mmmirac_44@hotmail.com>
2024-01-02 05:06:27 +0300

    UPDATE

--- GO/1-2-3/3/main_test.go
+++ GO/1-2-3/3/main_test.go
@@ -6,13 +6,23 @@ import (
 
 // TestMostFrequent tests the mostFrequent function for accuracy.
 
-func TestMostFrequent(t *testing.T) {
+func TestMostFrequentExisting(t *testing.T) {
 	words := []string{"apple", "pie", "apple", "red", "red", "red"}
-	expected := "red"    // Expected output.
+	expected := "red" //!Expected output.
+	result := mostFrequent(words)
+	if result != expected {
+		t.Errorf("mostFrequent() = %v, want %v", result, expected)
+	}
+}
+
+//! NEW TEST
+
+func TestMostFrequentNew(t *testing.T) {
+	words := []string{"a", "a", "a", "c", "b", "c", "c"}
+	expected := "a" // new test .
 
 	result := mostFrequent(words)
-	//if result
-	if result != expected { 
+	if result != expected {
 		t.Errorf("mostFrequent() = %v, want %v", result, expected)
 	}
 }
